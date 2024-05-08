3a3828e870047fb8aacff4b2a567c283ddc159d8
Saad Riazuddin Mahmood <saadriaz999@gmail.com>
2024-05-09 04:26:06 +0500

    kubernetes done

--- src/python/App.py
+++ src/python/App.py
@@ -1,7 +1,7 @@
 import socket
 from flask import Flask, request, render_template
 
-from utils import ChatbotUtils, EmbeddingUtils, QuestionAnswerUtils
+# from utils import ChatbotUtils, EmbeddingUtils, QuestionAnswerUtils
 
 
 socket.getaddrinfo('localhost', 5000)
@@ -12,8 +12,8 @@ EMBEDDING_MODEL_NAME = 'gemini'
 COLLECTION_NAME = 'gemini-2000-cosine'
 TOP_K_RESULTS_FROM_EMBEDDING_SEARCH = 3
 
-CHATBOT_MODEL = ChatbotUtils.load_chatbot(CHATBOT_MODEL_NAME)
-EMBEDDING_MODEL = EmbeddingUtils.load_embedding_model(EMBEDDING_MODEL_NAME)
+# CHATBOT_MODEL = ChatbotUtils.load_chatbot(CHATBOT_MODEL_NAME)
+# EMBEDDING_MODEL = EmbeddingUtils.load_embedding_model(EMBEDDING_MODEL_NAME)
 
 
 app = Flask(__name__)
@@ -29,9 +29,9 @@ def search():
     user_input = request.form['user_input']
     print(user_input)
 
-    prompt = QuestionAnswerUtils.rag(user_input, COLLECTION_NAME, top_k=TOP_K_RESULTS_FROM_EMBEDDING_SEARCH)
+    # prompt = QuestionAnswerUtils.rag(user_input, COLLECTION_NAME, top_k=TOP_K_RESULTS_FROM_EMBEDDING_SEARCH)
     # return render_template('search.html')
-    return prompt
+    return user_input
 
 
 if __name__ == '__main__':
